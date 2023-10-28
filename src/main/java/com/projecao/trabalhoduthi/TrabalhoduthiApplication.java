3d5f681d1e95cfba2c1793755ebee439c2ecb84e
Thiago Cavalcante Eloy <118282482+thiagouzi@users.noreply.github.com>
2023-10-28 20:20:12 -0300

    first commit

---
+++ src/main/java/com/projecao/trabalhoduthi/TrabalhoduthiApplication.java
@@ -0,0 +1,13 @@
+package com.projecao.trabalhoduthi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoduthiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoduthiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projecao/trabalhoduthi/TrabalhoduthiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecao.trabalhoduthi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoduthiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
