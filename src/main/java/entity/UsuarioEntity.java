fef305a9cb405ff6e0e23f373debbde87cf3341a
Thiago Cavalcante Eloy <118282482+thiagouzi@users.noreply.github.com>
2023-10-29 22:12:18 -0300

    2atualizacao

---
+++ src/main/java/entity/UsuarioEntity.java
@@ -0,0 +1,61 @@
+package entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "DT_ClIENTE")
+public class UsuarioEntity {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false, unique = true)
+	private String cpf;
+	@Column(nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private String nome; 
+	@Column(nullable = false)
+	private LocalDate dataNascimento;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	
+}
